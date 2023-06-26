c19bab01b4228c0bc6d03b56277c5361241a880c
Vladimir Jaward Hancco Soncco <vhanccos@unsa.edu.pe>
2023-06-26 18:20:42 -0500

    Cambios menores

--- NodeAVL.java
+++ NodeAVL.java
@@ -1,49 +1,53 @@
-public class NodeAVL<E> { 
+public class NodeAVL<E> {
 	private E data;
-	private NodeAVL<E> left; 
+	private NodeAVL<E> left;
 	private NodeAVL<E> right;
 	private int bf;
-	    
-	public NodeAVL(E data, NodeAVL<E> left, NodeAVL<E> right){
+
+	public NodeAVL(E data, NodeAVL<E> left, NodeAVL<E> right) {
 		this.data = data;
-	    this.left = left;
-	    this.right = right;
-	    this.bf = 0;
+		this.left = left;
+		this.right = right;
+		this.bf = 0;
 	}
-    public NodeAVL(E data){ 
-		this(data,null, null);
+
+	public NodeAVL(E data) {
+		this(data, null, null);
 	}
+
 	public int getBf() {
 		return bf;
 	}
+
 	public void setBf(int bf) {
 		this.bf = bf;
 	}
-    public E getData() {
+
+	public E getData() {
 		return this.data;
 	}
 
 	public void setData(E data) {
 		this.data = data;
 	}
-    public NodeAVL<E> getLeft() {
+
+	public NodeAVL<E> getLeft() {
 		return this.left;
 	}
 
 	public void setLeft(NodeAVL<E> left) {
 		this.left = left;
 	}
-    public NodeAVL<E> getRight() {
+
+	public NodeAVL<E> getRight() {
 		return this.right;
 	}
 
 	public void setRight(NodeAVL<E> right) {
 		this.right = right;
 	}
-    public String toString() {
+
+	public String toString() {
 		return this.data.toString() + "(" + this.bf + ")";
 	}
 }
-
-
-
--- arbolAVL.java
+++ arbolAVL.java
@@ -5,125 +5,134 @@ import myExceptions.ExceptionNoFound;
 public class arbolAVL<E extends Comparable<E>> {
 	private NodeAVL<E> root;
 	private boolean height; // bandera la cual nos indicara si debe realizarse modificaciones
-	
+
 	public arbolAVL() {
 		this.root = null;
 	}
+
 	public boolean isEmpty() {
 		return this.root == null;
 	}
+
 	public E getRoot() {
 		return this.root.getData();
 	}
+
 	private boolean isLeaf(NodeAVL<E> current) { // elemento es hoja
 		return current.getLeft() == null && current.getRight() == null;
 	}
+
 	public NodeAVL<E> parent(E x) throws ExceptionNoFound {
-		 NodeAVL<E> aux = parent(x, this.root);
-		 if (aux == null)
+		NodeAVL<E> aux = parent(x, this.root);
+		if (aux == null)
 			throw new ExceptionNoFound("Elemento no se encuentra en el arbol");
-                 System.out.println(aux.getData());
-		 return aux;
+		System.out.println(aux.getData());
+		return aux;
 	}
-	private NodeAVL<E> parent(E x, NodeAVL<E> current) throws ExceptionNoFound{
-		   if (current == null) {
-        throw new ExceptionNoFound("El árbol está vacío");
-    }
-    
-    NodeAVL<E> parent = null;
-    NodeAVL<E> node = current;
-    
-    while (node != null) {
-        int comparison = x.compareTo(node.getData());
-        
-        if (comparison == 0) {
-            if (parent == null) {
-                throw new ExceptionNoFound("El nodo insertado es la raíz del árbol");
-            }
-            return parent;
-        } else if (comparison < 0) {
-            parent = node;
-            node = node.getLeft();
-        } else {
-            parent = node;
-            node = node.getRight();
-        }
-    }
-    
-    throw new ExceptionNoFound("El elemento no se encuentra en el árbol");}
-	
+
+	private NodeAVL<E> parent(E x, NodeAVL<E> current) throws ExceptionNoFound {
+		if (current == null) {
+			throw new ExceptionNoFound("El árbol está vacío");
+		}
+
+		NodeAVL<E> parent = null;
+		NodeAVL<E> node = current;
+
+		while (node != null) {
+			int comparison = x.compareTo(node.getData());
+
+			if (comparison == 0) {
+				if (parent == null) {
+					throw new ExceptionNoFound("El nodo insertado es la raíz del árbol");
+				}
+				return parent;
+			} else if (comparison < 0) {
+				parent = node;
+				node = node.getLeft();
+			} else {
+				parent = node;
+				node = node.getRight();
+			}
+		}
+
+		throw new ExceptionNoFound("El elemento no se encuentra en el árbol");
+	}
+
 	public void insert(E x) throws ExceptionNoFound {
-		this.root = insert(x, this.root);	
+		this.root = insert(x, this.root);
 		this.height = false;
 	}
-	private NodeAVL<E> insert(E x, NodeAVL<E> current) throws ExceptionNoFound{
+
+	private NodeAVL<E> insert(E x, NodeAVL<E> current) throws ExceptionNoFound {
 		NodeAVL<E> res = current;
 		if (current == null) {
 			res = new NodeAVL<E>(x);
 			this.height = true;
-		}
-		else {
+		} else {
 			int resC = current.getData().compareTo(x);
 			if (resC == 0)
 				throw new ExceptionNoFound("El elemento ya se encuentra en el arbol");
 			if (resC < 0) {
 				res.setRight(insert(x, current.getRight()));
 				if (this.height) {
-					switch(res.getBf()) {
-					case -1: res.setBf(0); //-1+1
+					switch (res.getBf()) {
+						case -1:
+							res.setBf(0); // -1+1
 							this.height = false;
 							break;
-					case 0: res.setBf(1); //0+1
+						case 0:
+							res.setBf(1); // 0+1
 							break;
-					case 1: //res.setBf(2); 1+1
+						case 1: // res.setBf(2); 1+1
 							res = balanceToLeft(res);
 							this.height = false;
 							break;
 					}
 				}
-				
-			}
-			else { // resC > 0
+
+			} else { // resC > 0
 				res.setLeft(insert(x, current.getLeft()));
 				if (this.height) {
-					switch(res.getBf()) {
-					case -1: //res.setBf(-2); //-1-1
+					switch (res.getBf()) {
+						case -1: // res.setBf(-2); //-1-1
 							res = balanceToRight(res);
 							this.height = false;
 							break;
-					case 0: res.setBf(-1); //0-1
+						case 0:
+							res.setBf(-1); // 0-1
 							break;
-					case 1: res.setBf(0); // 1-1
+						case 1:
+							res.setBf(0); // 1-1
 							this.height = false;
 							break;
 					}
 				}
 			}
 		}
-		return res;	
+		return res;
 	}
-	private NodeAVL<E> balanceToLeft(NodeAVL<E> node){
+
+	private NodeAVL<E> balanceToLeft(NodeAVL<E> node) {
 		NodeAVL<E> son = node.getRight();
-		if (son.getBf() == 1){
+		if (son.getBf() == 1) {
 			node.setBf(0);
 			son.setBf(0);
 			node = rotateRSL(node);
-		}
-		else if (son.getBf() == -1) {
+		} else if (son.getBf() == -1) {
 			NodeAVL<E> gSon = son.getLeft();
-			switch(gSon.getBf()) {
-			case -1: 
-				node.setBf(0); 
-				son.setBf(-1); 
-				break;
-			case 0: 
-				node.setBf(0); 
-				son.setBf(0); 
-				break;
-			case 1: 
-				node.setBf(1); 
-				son.setBf(0); 
-				break;
+			switch (gSon.getBf()) {
+				case -1:
+					node.setBf(0);
+					son.setBf(-1);
+					break;
+				case 0:
+					node.setBf(0);
+					son.setBf(0);
+					break;
+				case 1:
+					node.setBf(1);
+					son.setBf(0);
+					break;
 			}
 			gSon.setBf(0);
 			node.setRight(rotateRSR(son));
@@ -131,64 +140,65 @@ public class arbolAVL<E extends Comparable<E>> {
 		}
 		return node;
 	}
-    private NodeAVL<E> balanceToRight(NodeAVL<E> node){
+
+	private NodeAVL<E> balanceToRight(NodeAVL<E> node) {
 		NodeAVL<E> son = node.getLeft();
-		if (son.getBf() == -1){
+		if (son.getBf() == -1) {
 			node.setBf(0);
 			son.setBf(0);
 			node = rotateRSR(node);
-		}
-		else if (son.getBf() == 1) {
+		} else if (son.getBf() == 1) {
 			NodeAVL<E> gSon = son.getRight();
-			switch(gSon.getBf()) {
-			case 1: 
-				node.setBf(0); 
-				son.setBf(-1); 
-				break;
-			case 0: 
-				node.setBf(0); 
-				son.setBf(0); 
-				break;
-			case -1: 
-				node.setBf(1); 
-				son.setBf(0); 
-				break;
+			switch (gSon.getBf()) {
+				case 1:
+					node.setBf(0);
+					son.setBf(-1);
+					break;
+				case 0:
+					node.setBf(0);
+					son.setBf(0);
+					break;
+				case -1:
+					node.setBf(1);
+					son.setBf(0);
+					break;
 			}
 			gSon.setBf(0);
-			
+
 			node.setLeft(rotateRSL(son));
 			node = rotateRSR(node);
 		}
 		return node;
-}
-	private NodeAVL<E> rotateRSL(NodeAVL<E> node){
+	}
+
+	private NodeAVL<E> rotateRSL(NodeAVL<E> node) {
 		NodeAVL<E> son = node.getRight();
 		node.setRight(son.getLeft());
 		son.setLeft(node);
 		node = son;
 		return node;
 	}
-	
-	private NodeAVL<E> rotateRSR(NodeAVL<E> node){
+
+	private NodeAVL<E> rotateRSR(NodeAVL<E> node) {
 		NodeAVL<E> son = node.getLeft();
 		node.setLeft(son.getRight());
 		son.setRight(node);
 		node = son;
 		return node;
 	}
-	
+
 	public NodeAVL<E> search(E x) throws ExceptionNoFound {
-		 NodeAVL<E> aux = search(x, this.root);
-		 if (aux == null) {
+		NodeAVL<E> aux = search(x, this.root);
+		if (aux == null) {
 			throw new ExceptionNoFound("Elemento no se encuentra en el arbol");
-		 }
-		 return aux;
-	}
-	private NodeAVL<E> search(E x, NodeAVL<E> current) throws ExceptionNoFound{
-		if (current == null) { // deja la recursividad si ya no hay elementos
-			return null; 
 		}
-		else {
+		return aux;
+	}
+
+	private NodeAVL<E> search(E x, NodeAVL<E> current) throws ExceptionNoFound {
+		if (current == null) { // deja la recursividad si ya no hay elementos
+			return null;
+		} else {
 			int resC = current.getData().compareTo(x);
 			if (resC == 0) // deja la recursividad hasta encontrar el elemento
 				return current;
@@ -198,49 +208,53 @@ public class arbolAVL<E extends Comparable<E>> {
 				return search(x, current.getLeft());
 		}
 	}
+
 	public void remove(E x) throws ExceptionNoFound {
-		this.root = remove(x, this.root);	
+		this.root = remove(x, this.root);
 	}
-	private NodeAVL<E> remove(E x, NodeAVL<E> current) throws ExceptionNoFound{
+
+	private NodeAVL<E> remove(E x, NodeAVL<E> current) throws ExceptionNoFound {
 		NodeAVL<E> res = current;
 		if (current == null) {
 			throw new ExceptionNoFound("Elemento no se encuentra en el arbol");
-		}
-		else {
-			
+		} else {
+
 		}
 		return res;
 
 	}
-	public NodeAVL<E> getMin(){
+
+	public NodeAVL<E> getMin() {
 		return getMin(root);
 	}
-	public NodeAVL<E> getMin(NodeAVL<E> nodo){
-	        if (nodo.getLeft()== null) {
-	            return nodo;
-	        }
-        	return getMin(nodo.getLeft());
+
+	public NodeAVL<E> getMin(NodeAVL<E> nodo) {
+		if (nodo.getLeft() == null) {
+			return nodo;
+		}
+		return getMin(nodo.getLeft());
 	}
-	
-	public NodeAVL<E> getMax(){
+
+	public NodeAVL<E> getMax() {
 		return getMax(root);
 	}
-	public NodeAVL<E> getMax(NodeAVL<E> nodo){
-	        if (nodo.getRight()== null) {
-	            return nodo;
-	        }
-       		return getMax(nodo.getRight());
+
+	public NodeAVL<E> getMax(NodeAVL<E> nodo) {
+		if (nodo.getRight() == null) {
+			return nodo;
+		}
+		return getMax(nodo.getRight());
 	}
-	
-	public ArrayList<NodeAVL<E>> son (E dato) throws ExceptionNoFound{
-		NodeAVL <E> nodo = search(dato);
-	        ArrayList<NodeAVL<E>> hijos = new ArrayList<>();
-	        if (nodo.getLeft() != null) {
-	            hijos.add(nodo.getLeft());
-	        }
-	        if (nodo.getRight() != null) {
-	            hijos.add(nodo.getRight());
-	        }
+
+	public ArrayList<NodeAVL<E>> son(E dato) throws ExceptionNoFound {
+		NodeAVL<E> nodo = search(dato);
+		ArrayList<NodeAVL<E>> hijos = new ArrayList<>();
+		if (nodo.getLeft() != null) {
+			hijos.add(nodo.getLeft());
+		}
+		if (nodo.getRight() != null) {
+			hijos.add(nodo.getRight());
+		}
 		return hijos;
 	}
 
@@ -248,24 +262,20 @@ public class arbolAVL<E extends Comparable<E>> {
 	public void inOrden() {
 		if (isEmpty()) {
 			System.out.println("Arbol esta vacío ....");
-		}
-		else{
+		} else {
 			inOrden(this.root);
 			System.out.println();
 		}
 	}
-	
+
 	private void inOrden(NodeAVL<E> current) {
 		if (current.getLeft() != null) {
 			inOrden(current.getLeft());
 		}
 		System.out.print(current + ", ");
-		if (current.getRight()!= null) {
+		if (current.getRight() != null) {
 			inOrden(current.getRight());
 		}
 	}
-	
-
-
 
 }
