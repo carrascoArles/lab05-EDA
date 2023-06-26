e07ee27684b01458ed52c214aa5afa2cd3fcb74d
JeanChara <jcharaco@unsa.edu.pe>
2023-06-26 09:17:45 -0500

    creacion clase arbolAvl y constructor

---
+++ arbolAvl.java
@@ -0,0 +1,13 @@
+import myExceptions.ExceptionNoFound;
+
+public class arbolAvl<E extends Comparable<E>> {
+	private NodeAvl<E> root;
+	private boolean height; // bandera la cual nos indicara si debe realizarse modificaciones
+	
+	public arbolAvl() {
+		this.root = null;
+	}
+
+
+
+}
